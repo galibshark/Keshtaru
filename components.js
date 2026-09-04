3e0127e9da2b7d6f6ff34df2a411d840af4f4ca5
Galib Rahman <galibrahman2005@gmail.com>
2026-09-04 23:57:33 +0600

    Update components.js

--- components.js
+++ components.js
@@ -1,11 +1,10 @@
-// পেজ লোড হওয়ার সাথে সাথে ন্যাভবার টেনে আনবে
 document.addEventListener("DOMContentLoaded", function() {
-    
-    // ন্যাভবার লোড করা
     fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-placeholder').innerHTML = data;
+            
+            // ম্যাজিক: ন্যাভবার পুরোপুরি লোড হওয়ার পর পুরো ওয়েবসাইটকে একটি সিগন্যাল দেওয়া হবে
+            document.dispatchEvent(new Event('NavbarLoaded'));
         });
-
 });
